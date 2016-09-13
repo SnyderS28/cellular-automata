842221bca6783b4feddb5b2d3cae3732910955c0
marzelin <marzelin@gmail.com>
2016-09-13 18:11:13 +0200

    add function descriptions in utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
+/**
+ * Utils module
+ * @module utils 
+ */
+
+/**
+ * This function creates a random row of 1's and 0's of a specified length
+ * @param {Number} length The length of a string
+ * @returns {String} Returns the binary string of 1's and 0's i.e. '101000' 
+ */
 export function createRandomRow(length: number): string {
     if (typeof length !== 'number') {
         throw new Error('length must be a number')
@@ -9,19 +19,42 @@ export function createRandomRow(length: number): string {
     return row
 }
 
+/**
+ * This function returns a random value, either 0 or 1
+ * @returns {String} randomly either 0 or 1
+ */
 function generateRandomBinaryValue(): number {
     return Math.floor(Math.random() * 2)
 }
 
+/** 
+ * This function creates the next row of cells based on the row given as an argument
+ * @param {String} originRow The row that is a basis for the row that will be returned
+ * @param {Array} activePatterns The list of patterns that if matched
+ * produce a living/active cell
+ * @returns {String} Returns a string of 0's and 1's that symbolizes a row of cells
+ */
 export function createNextRow(originRow: string, activePatterns: string[]):string {
-    let row = originRow
+    return originRow
         .split('')
         .map((v, i, values) => calculateValue(i, values, activePatterns))
         .join('')
-    return row
 }
 
-function calculateValue(originalIndex: number, originalRow: string[], activePatterns: string[]): string {
+/**
+ * This function calculates if a given cell should be active/living or not
+ * @param {Number} originalIndex The index of a current cell
+ * @param {Array} originalRow The row that contains the current cell ancestors
+ * which determine the state of this cell
+ * @param {Array} activePatterns The patterns that if matched indicate that
+ * the cell is active/alive
+ * @returns {String} either '1' if a cell is active/alive or '0' (inactive)
+ */
+function calculateValue(
+    originalIndex: number,
+    originalRow: string[],
+    activePatterns: string[]
+): string {
     const rowLength: number = originalRow.length
     const parentsRelativeIndexes: number[] = [-1, 0, 1]
     const pattern: string = parentsRelativeIndexes
@@ -33,6 +66,23 @@ function calculateValue(originalIndex: number, originalRow: string[], activePatt
         : '0'
 }
 
+/**
+ * This function converts relative indexes of the ancestor cells to the proper array indexes.
+ * By default each row of cells is perceived as a circle where the beginning of the row is
+ * connected with its end
+ * @param {Number} arrayLength The length of a row of cells
+ * @param {Number} originalIndex The index of a current cell
+ * @param {Number} relativeIndex The position of the desired cell relative to the current cell
+ * @returns {Number} The real index of a cell in a row
+ * @example
+ * 
+ * // if the index of the current cell is 0, and the desired cell
+ * // is on the left (relative index -1), the result will be the last index in a row
+ * convertRelativeToAbsoluteIndexes(5, 0, -1) // => 4
+ * // similarly if the index is the last cell in a row and
+ * the we search for the cell on the right (relative index 1), the result will be the first cell
+ * convertRelativeToAbsoluteIndexes(5, 4, 1) // => 0
+ */
 function convertRelativeToAbsoluteIndexes(
     arrayLength: number,
     originalIndex: number,
@@ -41,6 +91,14 @@ function convertRelativeToAbsoluteIndexes(
     return (arrayLength + originalIndex + relativeIndex) % arrayLength
 }
 
+/**
+ * This function creates a board with cells that adhere to a specified pattern
+ * @param {Number} length The number of rows the board will have
+ * @param {String} initialRow The first row of the board
+ * that will be used as a basis for creating next rows in the format of 1's and 0's i.e. '101'
+ * @param {Array} activePatterns The list of patterns that if matched
+ * produce a living/active cell 
+ */
 export function createBoard(
         length: number,
         initialRow: string,
@@ -59,6 +117,15 @@ export function createBoard(
     return board
 }
 
-function getLastItem(array: any[]) {
+/**
+ * This function gets the last item from the array given as an argument 
+ * @param {Array} array array from which to get the last item
+ * @example
+ * 
+ * const nums = [1, 2, 3]
+ * getLastItem(nums)
+ * // => 3
+ */
+function getLastItem<T>(array: T[]):T {
     return array[array.length -1]
 }
