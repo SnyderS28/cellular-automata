9f8ea630e3a24af2d142c4ec41497c8f339b10e3
marzelin <marzelin@gmail.com>
2016-09-13 12:11:07 +0200

    rename utils.test extension from js to ts for better tooling and add tests for createBoard fn

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,35 +1,39 @@
-const expect = require('expect')
+import {expect} from 'chai'
 
-import {createRandomRow, createNextRow} from './utils'
+import {
+    createRandomRow,
+    createNextRow,
+    createBoard
+} from './utils'
 
 describe('createRandomRow', () => {
     it('should return a string', function() {
-        expect(createRandomRow(10)).toBeA('string')
+        expect(createRandomRow(10)).to.be.a('string')
     })
     it('should return a string of a given length', function() {
-        expect(createRandomRow(5).length).toBe(5)
+        expect(createRandomRow(5).length).to.equal(5)
     })
     it('should return a string thatcontains only 1\'s and 0\'s', function() {
-        expect(createRandomRow(7)).toMatch(/[01]{7}/)
+        expect(createRandomRow(7)).to.match(/[01]{7}/)
     })
     it('should return an empty string if given 0 as an argument', function() {
-        expect(createRandomRow(0)).toBe('')
+        expect(createRandomRow(0)).to.equal('')
     })
     it('should throw an error if given not a number as an argument', function() {
-        expect(createRandomRow).toThrow()
+        expect(createRandomRow).to.throw()
     })
 })
 
 describe('createNextRow', () => {
     it('should return a string', function() {
         const actual = createNextRow('010', ['000'])
-        expect(actual).toBeA('string')
+        expect(actual).to.be.a('string')
     })
     it('should return a string of the same length as the original row', function() {
         const originString = '00000'
         const expected = originString.length
         const actual = createNextRow(originString, ['000'])
-        expect(actual.length).toBe(expected)
+        expect(actual.length).to.equal(expected)
     })
     it('should return a string that adheres to the pattern', function() {
         const tests = [
@@ -48,8 +52,24 @@ describe('createNextRow', () => {
         }]
         tests.forEach(({originRow, activePatterns, expected}) => {
             const actual = createNextRow(originRow, activePatterns)
-            expect(actual).toBe(expected)
+            expect(actual).to.equal(expected)
         })
     })
 }) 
+
+describe('createBoard', () => {
+    it('should return an array', function() {
+        const actual = createBoard(3, '000')
+        expect(actual).to.be.an('array')
+    })
+    it('should return an array of a given length', function() {
+        const actual = createBoard(5, '01010')
+        expect(actual.length).to.equal(5)
+    })
+    it('should return an array with the initial row as a first item', function() {
+        const initialRow = '010'
+        const actual = createBoard(3, initialRow)
+        expect(actual[0]).to.equal(initialRow)
+    })
+})
 
